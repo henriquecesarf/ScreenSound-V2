e76b3771787424e0561639045dc5a660599a1201
henriquecesarf <henriquecfdantas@gmail.com>
2024-03-22 15:38:39 -0300

    Melhorias do codo

---
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,19 @@
+﻿namespace ScreenSound.Modelos;
+
+internal class Avaliacao
+{
+    public Avaliacao(int nota)
+    {
+        Nota = nota;
+    }
+
+    public int Nota { get; }
+
+    public static Avaliacao Parse(string Texto)
+    {
+        int nota = int.Parse(Texto);
+        return new Avaliacao(nota);
+        
+    }
+
+}
